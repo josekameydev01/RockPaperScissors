9766532d036b09dd66cbf9807ee128d91e357ef4
josekameydev01 <gk01cracklitos@gmail.com>
2025-08-24 18:20:47 -0600

    first commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by carlosgalvankamey on 8/24/25.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
